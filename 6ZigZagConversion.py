be506608e0dbb2039906609ee7ca0a25a514079f
nijingxiong <525074588@qq.com>
2017-11-22 19:57:38 +0800

    lc No.6

---
+++ 6ZigZagConversion.py
@@ -0,0 +1,14 @@
+# l=[0 for i in range(2)]
+# print(l)
+# l=['a','b']
+# s=[]
+# s.append(l)
+# l=['c','d']
+# s.append(l)
+# for i in s:
+#     for j in i:
+#         print(j)
+# print(s,l)
+s=[['a', 0], [0, 'b'], ['c', 0], [0, 'd'], ['e', 0], [0, 'f']]
+print(len(s))
+print()
